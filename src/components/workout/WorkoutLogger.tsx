fab5cd8ede4a06e7a8c2dc0dd7913018aafbaccc
Aarushi Bajpai <aarushidigitalintern.recity@gmail.com>
2025-09-14 14:32:31 +0000

    Try fixing this error: `Runtime Error: Error: useFormContext is not defi

--- src/components/workout/WorkoutLogger.tsx
+++ src/components/workout/WorkoutLogger.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useForm, useFieldArray, Controller } from 'react-hook-form';
+import { useForm, useFieldArray, Controller, useFormContext } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '@/components/ui/button';
